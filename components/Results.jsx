85d1800148d8c715958a3d7feee44a48e6a2e21d
Malek Ghribi <ouassimghribi@gmail.com>
2022-02-17 07:52:45 +0100

    finished working on results and thunbnail components

---
+++ components/Results.jsx
@@ -0,0 +1,15 @@
+import {} from 'next';
+import Thumbnail from "./Thumbnail";
+
+
+function Results({ results }) {
+  return (
+    <div className="px-5 py-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
+      {results.results.map((result) => (
+        <Thumbnail key={result.id} result={result} />
+      ))}
+    </div>
+  );
+}
+
+export default Results;
--- pages/index.jsx
+++ pages/index.jsx
@@ -3,8 +3,10 @@ import Head from "next/head";
 //components imports
 import Header from "@/components/Header";
 import Nav from "@/components/Nav";
+import Results from "@/components/Results";
+import requests from "utils/requests";
 
-export default function Home() {
+export default function Home({ results }) {
   return (
     <div>
       <Head>
@@ -14,6 +16,23 @@ export default function Home() {
       </Head>
       <Header />
       <Nav />
+      <Results results={results} />
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+
+  const request = await fetch(
+    `https://api.themoviedb.org/3${
+      genre ? requests[genre]?.url : requests.fetchTrending.url
+    }`
+  ).then((res) => res.json());
+
+  return {
+    props: {
+      results: request,
+    },
+  };
 }
